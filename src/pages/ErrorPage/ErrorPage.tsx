606c978229cf1c00f770aa4331f714754531cb03
ww_side <daniil.metelia@gmail.com>
2023-06-13 19:28:13 +0300

    added error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,18 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage: FC = () => {
-  return <div>Page not found</div>;
+  return (
+    <div className="bg-zinc-800 flex items-center justify-center h-screen">
+      <div className="max-w-lg mx-auto p-8 bg-zinc-900 rounded shadow">
+        <h1 className="text-4xl text-white text-center mb-6">Error 404</h1>
+        <p className="text-xl text-white text-center mb-6">Page not found.</p>
+        <Link className="text-lg text-white text-center hover:underline" to="/">
+          Return to home page
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
