9768cc4657cd6ae47ed84b0a46447e506bf97dbb
ww_side <daniil.metelia@gmail.com>
2023-06-23 12:46:26 +0300

    fixed input in TimerInput

--- src/components/Timer/TimerInput/TimerInput.tsx
+++ src/components/Timer/TimerInput/TimerInput.tsx
@@ -8,7 +8,7 @@ interface ITimerInputProps {
 const TimerInput: FC<ITimerInputProps> = ({ inputRef, placeholder }) => {
   return (
     <input
-      className="text-black w-3/12 max-md:w-9/12 my-3 bg-transparent border
+      className="text-black w-3/12 max-lg:w-6/12 max-md:w-9/12 my-3 bg-transparent border
         border-gray-300 text-white placeholder-white::placeholder
         rounded-md px-4 py-2 focus:outline-none focus:ring-1
         focus:ring-white focus:border-white"
